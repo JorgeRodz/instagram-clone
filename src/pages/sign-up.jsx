7ac080a85c9d67c370f4db55d34d699d9f5f88c7
Jorge Rodriguez <jarf4321@gmail.com>
2023-10-22 14:39:49 -0600

    Add sign-up page funcionality

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -2,33 +2,57 @@ import { useState, useContext, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
+import { doesUsernameExist } from '../services/firebase';
 
-export default function Login() {
+export default function SignUp() {
   const navigate = useNavigate();
   const { firebase } = useContext(FirebaseContext);
 
+  const [username, setUsername] = useState('');
+  const [fullName, setFullName] = useState('');
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
 
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleLogin = async (event) => {
+  const handleSignup = async (event) => {
     event.preventDefault();
 
-    try {
-      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
-      navigate(ROUTES.DASHBOARD);
-    } catch (error) {
-      console.log(error);
-      setEmailAddress('');
-      setPassword('');
-      setError(error.message);
+    const usernameExists = await doesUsernameExist(username);
+    if (!usernameExists) {
+      try {
+        const createdUserResult = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(emailAddress, password);
+
+        await createdUserResult.user.updateProfile({
+          displayName: username,
+        });
+
+        await firebase.firestore().collection('users').add({
+          userId: createdUserResult.user.uid,
+          username: username.toLowerCase(),
+          fullName,
+          emailAddress: emailAddress.toLowerCase(),
+          following: [],
+          dateCreated: Date.now(),
+        });
+
+        navigate(ROUTES.DASHBOARD);
+      } catch (error) {
+        setFullName('');
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      }
+    } else {
+      setError('That username is already taken, please try another.');
     }
   };
 
   useEffect(() => {
-    document.title = 'Login - Instagram';
+    document.title = 'Sign up - Instagram';
   }, []);
 
   return (
@@ -50,7 +74,23 @@ export default function Login() {
           </h1>
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-          <form onSubmit={handleLogin} method="POST">
+          <form onSubmit={handleSignup} method="POST">
+            <input
+              aria-label="Enter your username"
+              type="text"
+              placeholder="Username"
+              className="text-sm text-gray-700 w-full mr-3 py-5 px-4 h-2 rounded border border-gray-primary mb-2"
+              onChange={({ target }) => setUsername(target.value)}
+              value={username}
+            />
+            <input
+              aria-label="Enter your full name"
+              type="text"
+              placeholder="Full Name"
+              className="text-sm text-gray-700 w-full mr-3 py-5 px-4 h-2 rounded border border-gray-primary mb-2"
+              onChange={({ target }) => setFullName(target.value)}
+              value={fullName}
+            />
             <input
               aria-label="Enter your email address"
               type="text"
@@ -65,7 +105,6 @@ export default function Login() {
               placeholder="Password"
               className="text-sm text-gray-700 w-full mr-3 py-5 px-4 h-2 rounded border border-gray-primary mb-2"
               onChange={({ target }) => setPassword(target.value)}
-              value={password}
             />
             <button
               disabled={isInvalid}
@@ -74,15 +113,15 @@ export default function Login() {
                 isInvalid && 'opacity-50'
               }`}
             >
-              Login
+              Sign up
             </button>
           </form>
         </div>
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 border rounded border-gray-primary">
           <p className="text-sm">
-            Don&apos;t have an account?{` `}
-            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
-              Sign up
+            Have an account?{` `}
+            <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
+              Login
             </Link>
           </p>
         </div>
